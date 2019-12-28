f7e1024fc73509580ac45835371ddef882fd16a7
jasshan29goel <goeljashan29@gmail.com>
2019-12-28 13:24:09 +0530

    Updates

---
+++ precache-manifest.985dd90e0258774ec72a06388fcbe37d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4a9298fd29667946a3673672041ef3",
+    "url": "/timetable/index.html"
+  },
+  {
+    "revision": "f3e1dc4463a78c5bcc1b",
+    "url": "/timetable/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "fee776dfa02cc59c5cdb",
+    "url": "/timetable/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "f3e1dc4463a78c5bcc1b",
+    "url": "/timetable/static/js/2.5920fdc2.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/timetable/static/js/2.5920fdc2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fee776dfa02cc59c5cdb",
+    "url": "/timetable/static/js/main.90693170.chunk.js"
+  },
+  {
+    "revision": "efb675770360fca38ce2",
+    "url": "/timetable/static/js/runtime-main.701552d7.js"
+  }
+]);
